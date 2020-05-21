c5ef65d5cf4e617c856f6a82bdcffc000d7c8b8c
ian <ian.aquino2003@gmail.com>
2020-05-21 13:06:28 -0300

    atualizei main

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var xhr = new XMLHttpRequest();
+var minhaPromisse = function(){
+    //resolve=sucesso reject=falha
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/SRevolutionYT');
-xhr.send(null);
-
-xhr.onreadystatechange = function(){
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.open('GET', 'https://api.github.com/users/SRevolutionYT');
+        xhr.send(null);
+        
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4){
+                if(xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                } else{
+                    reject('ERROR na requisição');
+                }
+            }
+        }
+    });
 }
+
+minhaPromisse();
+.then(function(response) {
+    console.log(response);
+})
+.catch(function(error) {});
